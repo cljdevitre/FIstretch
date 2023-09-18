f835ba42a16b546a91194cfc94a4992d522ea840
cljdevitre <103474333+cljdevitre@users.noreply.github.com>
2023-09-18 13:56:29 -0700

    unit test

--- Unit tests/test_RelaxiFI.py
+++ Unit tests/test_RelaxiFI.py
@@ -6,9 +6,9 @@ import RelaxiFI as relax
 decimalPlace=4
 class test_kmdepth(unittest.TestCase):
     def test_kmdepth(self):
-        self.assertAlmostEqual(relax.find_P_for_kmdepth(target_depth=5, 
+        self.assertAlmostEqual(round(relax.find_P_for_kmdepth(target_depth=5, 
 config=relax.config_crustalmodel(crust_dens_kgm3=2750),
-initial_P_guess=0, tolerance=0.1)[0], 1.3488,
+initial_P_guess=0, tolerance=0.1)[0],4), 1.3488,
 decimalPlace, "Calculated P doesnt match test value")
         
 
