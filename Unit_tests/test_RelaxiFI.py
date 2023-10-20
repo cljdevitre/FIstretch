d92aed81533d9395324973c7b28c87b5171a9332
cljdevitre <103474333+cljdevitre@users.noreply.github.com>
2023-10-20 14:30:59 -0700

    version update and unit test

--- Unit_tests/test_RelaxiFI.py
+++ Unit_tests/test_RelaxiFI.py
@@ -6,9 +6,9 @@ decimalPlace = 4
 class TestFindP4kmD(unittest.TestCase):
     def test_find_P_for_kmdepth(self):
         self.assertAlmostEqual(
-            round(relax.find_P_for_kmdepth(target_depth=5, 
+            round(relax.find_P_for_kmdepth(target_depth_km=5, 
             config=relax.config_crustalmodel(crust_dens_kgm3=2750),
-            initial_P_guess=0, tolerance=0.1)[0],4), 
+            initial_P_guess_kbar=0, tolerance=0.1)[0],4), 
             float(1.3489),
             decimalPlace,
             "Calculated P doesn't match test value"
